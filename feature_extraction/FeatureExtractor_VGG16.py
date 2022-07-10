60f9c40df0c911043b45d684a4a727d77b93fe9f
EnzoUbaldoPetrocco <48513075+EnzoUbaldoPetrocco@users.noreply.github.com>
2022-07-10 16:42:40 +0200

    Add files via upload

---
+++ feature_extraction/FeatureExtractor_VGG16.py
@@ -0,0 +1,114 @@
+#! /usr/bin/env python3
+from tabnanny import verbose
+import manipulating_images_better
+import numpy as np
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input
+import cv2
+
+class FeatureExtractor:
+    def __init__(self):
+        model = VGG16(weights='imagenet', include_top=False)
+
+        itd = manipulating_images_better.ImagesToData()
+        itd.bf_ml()
+        CX = itd.CX
+        CXT = itd.CXT
+        CY = itd.CY
+        CYT = itd.CYT
+
+        FX = itd.FX
+        FXT = itd.FXT
+        FY = itd.FY
+        FYT = itd.FYT
+
+        MX = itd.MX
+        MXT = itd.MXT
+        MY = itd.MY
+        MYT = itd.MYT
+
+        #################################################
+        ############# FEATURE EXTRACTION ################
+        print('FEATURE EXTRACTION')
+        features = []
+        for i in CX:
+            x = np.reshape(i, (itd.size,itd.size))
+            x = cv2.merge([x,x,x])
+            x = image.img_to_array(x)
+            x = np.expand_dims(x, axis=0)
+            feature = model.predict(x, verbose = 0)
+            features.append(feature[0][0][0])
+        
+        self.CX = np.array(features)
+        self.CY = CY
+
+        features = []
+        for i in CXT:
+            x = np.reshape(i, (itd.size,itd.size))
+            x = cv2.merge([x,x,x])
+            x = image.img_to_array(x)
+            x = np.expand_dims(x, axis=0)
+            feature = model.predict(x, verbose = 0)
+            features.append(feature[0][0][0])
+        
+        self.CXT = np.array(features)
+        self.CYT = CYT
+
+        features = []
+        for i in FX:
+            x = np.reshape(i, (itd.size,itd.size))
+            x = cv2.merge([x,x,x])
+            x = image.img_to_array(x)
+            x = np.expand_dims(x, axis=0)
+            feature = model.predict(x, verbose = 0)
+            features.append(feature[0][0][0])
+        
+        self.FX = np.array(features)
+        self.FY = FY
+
+        features = []
+        for i in FXT:
+            x = np.reshape(i, (itd.size,itd.size))
+            x = cv2.merge([x,x,x])
+            x = image.img_to_array(x)
+            x = np.expand_dims(x, axis=0)
+            feature = model.predict(x, verbose = 0)
+            features.append(feature[0][0][0])
+        
+        self.FXT = np.array(features)
+        self.FYT = FYT
+
+        features = []
+        for i in MX:
+            x = np.reshape(i, (itd.size,itd.size))
+            x = cv2.merge([x,x,x])
+            x = image.img_to_array(x)
+            x = np.expand_dims(x, axis=0)
+            feature = model.predict(x, verbose = 0)
+            features.append(feature[0][0][0])
+        
+        self.MX = np.array(features)
+        self.MY = MY
+
+        features = []
+        for i in MXT:
+            x = np.reshape(i, (itd.size,itd.size))
+            x = cv2.merge([x,x,x])
+            x = image.img_to_array(x)
+            x = np.expand_dims(x, axis=0)
+            feature = model.predict(x, verbose = 0)
+            features.append(feature[0][0][0])
+        
+        self.MXT = np.array(features)
+        self.MYT = MYT
+
+        
+
+
+
+
+
+
+
+
