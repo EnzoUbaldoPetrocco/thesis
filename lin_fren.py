2a9409ea54727f875289c1b5f853a92f8779a42f
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2022-11-08 11:47:44 +0100

    deleted file from Pepper branch

--- lin_fren.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-from classificator import SVCClassificator
-kernel = [ 'linear', 'rbf' ]
-ds_selections = ['chinese', 'french', 'mix']
-linear_french_svc_classificator = SVCClassificator()
-linear_french_svc_classificator.ds_selection = ds_selections[1]
-linear_french_svc_classificator.kernel = kernel[0]
-linear_french_svc_classificator.execute()
--- rbf_mix.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-from classificator import SVCClassificator
-kernel = [ 'linear', 'rbf' ]
-ds_selections = ['chinese', 'french', 'mix']
-gaussian_mix_svc_classificator = SVCClassificator()
-gaussian_mix_svc_classificator.ds_selection = ds_selections[2]
-gaussian_mix_svc_classificator.kernel = kernel[1]
-gaussian_mix_svc_classificator.execute()
