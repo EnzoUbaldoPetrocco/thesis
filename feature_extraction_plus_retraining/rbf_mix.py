b97d4ef0601cfa84a1b9abc939170d94422e888f
EnzoUbaldoPetrocco <enzopetrocco@hotmail.it>
2022-11-08 11:45:14 +0100

    Deleted file from Choregraphe branch

--- feature_extraction_plus_retraining/rbf_mix.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-from classificator_featureextraction import SVCClassificator
-kernel = [ 'linear', 'rbf' ]
-ds_selections = ['chinese', 'french', 'mix']
-gaussian_chinese_svc_classificator = SVCClassificator(ds_selections[2])
-gaussian_chinese_svc_classificator.ds_selection = ds_selections[2]
-gaussian_chinese_svc_classificator.kernel = kernel[1]
-gaussian_chinese_svc_classificator.execute()
--- little_mixed/mlin_chin.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-from mclassificator import SVCClassificator
-kernel = [ 'linear', 'rbf' ]
-ds_selections = ['chinese', 'french']
-gaussian_chinese_svc_classificator = SVCClassificator()
-gaussian_chinese_svc_classificator.ds_selection = ds_selections[0]
-gaussian_chinese_svc_classificator.kernel = kernel[0]
-gaussian_chinese_svc_classificator.execute()
